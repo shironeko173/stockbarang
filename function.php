2caa5738d803cad963fe05c88be1df6b008ea758
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-14 03:27:20 +0700

    test

--- function.php
+++ function.php
@@ -12,7 +12,7 @@ $conn = mysqli_connect("mysql.railway.internal","root","UhMzqiSqKTqYJuSJxNMOuHvR
         }
     }
 
-        $folder = __DIR__ . '/storage_railway';
+        $folder = __DIR__ . '/storage';
 
         // Cek apakah folder ada
         if (file_exists($folder)) {
