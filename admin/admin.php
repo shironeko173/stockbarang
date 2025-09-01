654e67c9a03fad7c1b3fc836093a87d4a8b7ebbd
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-09-02 00:21:19 +0700

    tset

--- admin/admin.php
+++ admin/admin.php
@@ -30,7 +30,7 @@ $email = $_SESSION['email'];
     </head>
     <body class="sb-nav-fixed">
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <!-- Navbar Brand-->
+            <!-- Navbar Branded-->
             <a class="navbar-brand ps-3" href="home.php"><i class="fas fa-dolly-flatbed me-2 mb-1"></i><b>STOCK BARANG</b></a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
